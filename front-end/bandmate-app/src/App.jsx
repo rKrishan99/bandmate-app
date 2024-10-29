d13b1a98268282613139e44640586049184ea80c
RajithaKWG <rkrishan894@gmail.com>
2024-10-30 03:03:10 +0530

    Created components and pages

--- front-end/bandmate-app/src/App.jsx
+++ front-end/bandmate-app/src/App.jsx
@@ -1,9 +1,19 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Contact from "./pages/contact/Contact";
+import NotFound from "./pages/notFound/NotFound";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Bandmate App!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   );
 }
---
+++ front-end/bandmate-app/src/components/navbar/LandingNav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LandingNav = () => {
+  return (
+    <div>
+        <nav>
+            
+        </nav>
+    </div>
+  )
+}
+
+export default LandingNav
