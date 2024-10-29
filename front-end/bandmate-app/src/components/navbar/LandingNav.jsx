a2aaf3c3e2e5dc08ba5dfaa1a1fedbb3d43cdbb8
RajithaKWG <rkrishan894@gmail.com>
2024-10-30 03:32:04 +0530

    Creating navbar

--- front-end/bandmate-app/src/components/navbar/LandingNav.jsx
+++ front-end/bandmate-app/src/components/navbar/LandingNav.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import "../../../public/Bandmate_logo.png";
 
 const LandingNav = () => {
   return (
-    <div>
-        <nav>
-            
-        </nav>
+    <div className="flex items-center bg-blue-700">
+      <div className="flex-1">
+        <Link to="/">
+          <img className="w-52" src="./Bandmate_logo.png" alt="Bandmate Logo" />
+        </Link>
+      </div>
+      <div className="flex-2">
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Home
+        </NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+      </div>
+      <div className="flex-1">
+        <Link to="/login">
+          <button>Signin</button>
+        </Link>
+        <Link to="/signup">
+            <button>Signup</button>
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LandingNav
+export default LandingNav;
--- front-end/bandmate-app/src/pages/home/Home.jsx
+++ front-end/bandmate-app/src/pages/home/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import LandingNav from '../../components/navbar/LandingNav'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <LandingNav />
+        <h1>This is Home</h1>
+    </div>
   )
 }
 
