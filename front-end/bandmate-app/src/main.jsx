bc7bb1f931f89bd89852ca19e5d7a5e15e05fcff
RajithaKWG <rkrishan894@gmail.com>
2024-11-03 02:53:17 +0530

    login fixed

--- front-end/bandmate-app/src/main.jsx
+++ front-end/bandmate-app/src/main.jsx
@@ -6,17 +6,22 @@ import { LoginProvider } from "./context/loginContext/LoginContext.jsx";
 import { RegisterProvider } from "./context/registerContext/RegisterContext.jsx";
 import { BandRegisterProvider } from "./context/bandRegisterContext/BandRegisterContext.jsx";
 import { MusicianRegisterProvider } from "./context/musicianRegisterContext/MusicianRegisterContext.jsx";
+import { CurrentUserProvider } from "./context/currentUserContext/CurrentUserContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <RegisterProvider>
-    <MusicianRegisterProvider>
-      <BandRegisterProvider>
-        <LoginProvider>
-          <Router>
-            <App />
-          </Router>
-        </LoginProvider>
-      </BandRegisterProvider>
-    </MusicianRegisterProvider>
-  </RegisterProvider>
+  
+    <RegisterProvider>
+      <MusicianRegisterProvider>
+        <BandRegisterProvider>
+        <CurrentUserProvider>
+          <LoginProvider>
+            <Router>
+              <App />
+            </Router>
+          </LoginProvider>
+          </CurrentUserProvider>
+        </BandRegisterProvider>
+      </MusicianRegisterProvider>
+    </RegisterProvider>
+  
 );
